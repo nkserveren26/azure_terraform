5d3cf7b20f604146e71da2c734b89067aba88d21
nkserveren26 <norinoritoshi@outlook.jp>
2023-12-11 15:31:38 +0900

    add subnet configuration

--- vnet.tf
+++ vnet.tf
@@ -15,3 +15,10 @@ resource "azurerm_virtual_network" "testvnet" {
   location = azurerm_resource_group.testresourcegroup.location
   resource_group_name = azurerm_resource_group.testresourcegroup.name
 }
+
+resource "azurerm_subnet" "test_subnet" {
+  name = "testsubnet"
+  resource_group_name = azurerm_resource_group.testresourcegroup.name
+  virtual_network_name = azurerm_virtual_network.testvnet.name
+  address_prefixes = [ "10.0.0.0/24" ]
+}
