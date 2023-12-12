a0d029b44c53d317ea2a25cc08613a5ed05f52b0
nkserveren26 <norinoritoshi@outlook.jp>
2023-12-12 22:52:11 +0900

    commit

--- vm.tf
+++ vm.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_security_group" "testvmnsg" {
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges    = ["22"]
     source_address_prefixes    = ["192.168.1.1"]
