18003a460d21ec55060a7c22348f99725fbe04d8
nkserveren26 <norinoritoshi@outlook.jp>
2023-12-14 23:22:32 +0900

    commit

--- vm.tf
+++ vm.tf
@@ -62,14 +62,14 @@ resource "azurerm_virtual_machine" "testvm" {
   name = "${replace(var.disk_names[count.index], "-disk", "")}"
   location = "${azurerm_resource_group.testresourcegroup.location}"
   resource_group_name = "${azurerm_resource_group.testresourcegroup.name}"
-  network_interface_ids = [ azurerm_network_interface.testvmnic.id ]
+  network_interface_ids = [ azurerm_network_interface.testvmnics[count.index].id ]
 
   # OSディスクの情報
   storage_os_disk {
-    name = data.azurerm_managed_disk.testvm_disk.name
+    name = data.azurerm_managed_disk.managed_disks[count.index].name
     caching = "ReadWrite"
     create_option = "Attach"
-    managed_disk_id = data.azurerm_managed_disk.testvm_disk.id
+    managed_disk_id = data.azurerm_managed_disk.managed_disks[count.index].id
     os_type = "Windows"
   }
   vm_size = "Standard_DS1_v2"
